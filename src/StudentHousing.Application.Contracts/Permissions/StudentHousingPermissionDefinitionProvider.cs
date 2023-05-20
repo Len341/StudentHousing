f9573d21d5e90598bc91900b3534a81f10a724c8
Leonard Botha <leonard@cog3nt.com>
2023-05-20 06:13:45 +0200

    Add housing permissions

--- src/StudentHousing.Application.Contracts/Permissions/StudentHousingPermissionDefinitionProvider.cs
+++ src/StudentHousing.Application.Contracts/Permissions/StudentHousingPermissionDefinitionProvider.cs
@@ -8,9 +8,14 @@ namespace StudentHousing.Permissions
     {
         public override void Define(IPermissionDefinitionContext context)
         {
-            var myGroup = context.AddGroup(StudentHousingPermissions.GroupName);
-            //Define your own permissions here. Example:
-            //myGroup.AddPermission(StudentHousingPermissions.MyPermission1, L("Permission:MyPermission1"));
+            var StudentHousingGroup = context.AddGroup(StudentHousingPermissions.GroupName);
+
+            var grpHousing = StudentHousingGroup.AddPermission(StudentHousingPermissions.Housing.Default, L("Permission:StudentHousing.Housing.Default"));
+            grpHousing.AddChild(StudentHousingPermissions.Housing.Create, L("Permission:StudentHousing.Housing.Create"));
+            grpHousing.AddChild(StudentHousingPermissions.Housing.View, L("Permission:StudentHousing.Housing.View"));
+            grpHousing.AddChild(StudentHousingPermissions.Housing.Update, L("Permission:StudentHousing.Housing.Update"));
+            grpHousing.AddChild(StudentHousingPermissions.Housing.Delete, L("Permission:StudentHousing.Housing.Delete"));
+
         }
 
         private static LocalizableString L(string name)
--- src/StudentHousing.Application.Contracts/Permissions/StudentHousingPermissions.cs
+++ src/StudentHousing.Application.Contracts/Permissions/StudentHousingPermissions.cs
@@ -6,5 +6,13 @@
 
         //Add your own permission names. Example:
         //public const string MyPermission1 = GroupName + ".MyPermission1";
+        public class Housing
+        {
+            public const string Default = GroupName + ".Housing";
+            public const string View = Default + ".View";
+            public const string Create = Default + ".Create";
+            public const string Update = Default + ".Update";
+            public const string Delete = Default + ".Delete";
+        }
     }
 }
